287f06fef48a866c2d1d53d90c86dd7310c4938b
prabhjotps35ps-alt <prabhjot.ps35.ps@gmail.com>
2026-05-27 19:16:12 +0530

    Create position_manager.py

---
+++ position_manager.py
@@ -0,0 +1,25 @@
+from delta_client import DeltaClient
+
+delta = DeltaClient()
+
+
+class PositionManager:
+    def has_open_position(self, symbol):
+        try:
+            positions = delta.get_positions()
+
+            if not positions:
+                return False
+
+            result = positions.get("result", [])
+
+            for pos in result:
+                if pos.get("product_symbol") == symbol:
+                    size = abs(float(pos.get("size", 0)))
+                    if size > 0:
+                        return True
+
+            return False
+
+        except Exception:
+            return False
