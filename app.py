1f18f9e784bb5d5ba4741bc37bedc2ad7355e60d
prabhjotps35ps-alt <prabhjot.ps35.ps@gmail.com>
2026-05-28 16:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-
-    return jsonify({
-        "status": "running"
-    })
-
+    return "Bot Running"
 
 @app.route("/health")
 def health():
-
-    return jsonify({
-        "health": "ok"
-    })
-
+    return {
+        "status": "ok"
+    }
 
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=10000
-    )
+    app.run(host="0.0.0.0", port=10000)
