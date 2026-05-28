577e95e652a5f5d64e9c74b2efa0e653df9b7963
prabhjotps35ps-alt <prabhjot.ps35.ps@gmail.com>
2026-05-28 16:53:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from delta_client import DeltaClient
 
 app = Flask(__name__)
 
+delta = DeltaClient()
+
+
 @app.route("/")
 def home():
+
     return "Bot Running"
+
 
 @app.route("/health")
 def health():
-    return {
+
+    return jsonify({
         "status": "ok"
-    }
+    })
+
+
+@app.route("/delta-test")
+def delta_test():
+
+    return jsonify(
+        delta.test()
+    )
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+
+    app.run(
+        host="0.0.0.0",
+        port=10000
+    )
